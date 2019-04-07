dc8a12edd92fa1b476b49825cd0585797994ec10
Giles Bowkett <gilesb@gmail.com>
2019-04-07 17:40:59 -0600

    word freq: cleanup (stopwords, capitalization)

--- lib/top_100.rb
+++ lib/top_100.rb
@@ -66,6 +66,37 @@ Commit = Struct.new(:sha1, :summary, :date, :message, :show) do
 end
 
 IndividualContributor = Struct.new(:commits, :start, :finish) do
+  STOPWORDS = [
+    'a','cannot','into','our','thus','about','co','is','ours','to','above',
+    'could','it','ourselves','together','across','down','its','out','too',
+    'after','during','itself','over','toward','afterwards','each','last','own',
+    'towards','again','eg','latter','per','under','against','either','latterly',
+    'perhaps','until','all','else','least','rather','up','almost','elsewhere',
+    'less','same','upon','alone','enough','ltd','seem','us','along','etc',
+    'many','seemed','very','already','even','may','seeming','via','also','ever',
+    'me','seems','was','although','every','meanwhile','several','we','always',
+    'everyone','might','she','well','among','everything','more','should','were',
+    'amongst','everywhere','moreover','since','what','an','except','most','so',
+    'whatever','and','few','mostly','some','when','another','first','much',
+    'somehow','whence','any','for','must','someone','whenever','anyhow',
+    'former','my','something','where','anyone','formerly','myself','sometime',
+    'whereafter','anything','from','namely','sometimes','whereas','anywhere',
+    'further','neither','somewhere','whereby','are','had','never','still',
+    'wherein','around','has','nevertheless','such','whereupon','as','have',
+    'next','than','wherever','at','he','no','that','whether','be','hence',
+    'nobody','the','whither','became','her','none','their','which','because',
+    'here','noone','them','while','become','hereafter','nor','themselves','who',
+    'becomes','hereby','not','then','whoever','becoming','herein','nothing',
+    'thence','whole','been','hereupon','now','there','whom','before','hers',
+    'nowhere','thereafter','whose','beforehand','herself','of','thereby','why',
+    'behind','him','off','therefore','will','being','himself','often','therein',
+    'with','below','his','on','thereupon','within','beside','how','once',
+    'these','without','besides','however','one','they','would','between','i',
+    'only','this','yet','beyond','ie','onto','those','you','both','if','or',
+    'though','your','but','in','other','through','yours','by','inc','others',
+    'throughout','yourself','can','indeed','otherwise','thru','yourselves'
+  ]
+
   def self.parse(raw_data)
     commits = raw_data["github_urls"].each_with_index.map do |url, index|
       sha1 = url.gsub('https://github.com/rails/rails/commit/', '')
@@ -85,7 +116,8 @@ IndividualContributor = Struct.new(:commits, :start, :finish) do
     words = (self.commits.map {|commit| commit.message.split(/\W/)}).flatten.select {|word| word != ""}
 
     words.inject(Hash.new(0)) do |acc, word|
-      acc[word] += 1
+      word.downcase!
+      acc[word] += 1 unless STOPWORDS.include?(word)
       acc
     end
   end

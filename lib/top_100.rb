19f8595e46777ac275ded92002d0d36ec2867c5f
Giles Bowkett <gilesb@gmail.com>
2019-04-06 19:44:55 -0600

    got the basic scrape working

--- lib/top_100.rb
+++ lib/top_100.rb
@@ -1,1 +1,20 @@
+require 'wombat'
 
+Scraper = Struct.new(:number) do
+  def scrape
+    first_run = Wombat.crawl do
+      base_url "https://contributors.rubyonrails.org"
+      path "/"
+
+      contributor_ranks "css=td.contributor-rank", :list
+      contributor_names "css=td.contributor-name", :list
+      contributor_links "xpath=//html/body/div[3]/div/div/div/div/div/table/tr/td[2]/a/@href", :list
+    end
+
+    # TODO: filter the lists in scraped; only return top 100
+    first_run.inject({}) do |h, (k, v)|
+      h[k] = v.first(100)
+      h
+    end
+  end
+end
--- spec/top_100_spec.rb
+++ spec/top_100_spec.rb
@@ -5,13 +5,50 @@ describe "args" do
 end
 
 describe "scraping the web" do
-  it "hits the main list web page"
+  before(:all) do
+    # cache it. hitting the network once is bad enough!
+    @scraped = Scraper.new(1).scrape
+  end
 
-  it "gets the committer's link"
+  it "hits the main list web page" do
+    expect(@scraped).to eq("wombat")
+  end
+
+  it "gets only the first 100 ranks" do
+    expect(@scraped["contributor_ranks"].count).to eq(100)
+  end
+
+  it "gets only the first 100 names" do
+    expect(@scraped["contributor_names"].count).to eq(100)
+  end
+
+  it "gets only the first 100 links" do
+    expect(@scraped["contributor_links"].count).to eq(100)
+  end
+
+  describe "per contributor" do
+    let(:contributor) do
+      {
+        rank: @scraped["contributor_ranks"][0],
+        link: @scraped["contributor_links"][0],
+        name: @scraped["contributor_names"][0]
+      }
+    end
+
+		it "gets the committer's rank" do
+      expect(contributor[:rank]).to eq("#1")
+    end
+
+		it "gets the committer's link" do
+      expect(contributor[:link]).to eq("/contributors/rafael-mendonca-franca/commits")
+    end
+
+		it "gets the committer's name" do
+      expect(contributor[:name]).to eq("Rafael Mendonça França")
+    end
+  end
 
   it "hits the committer's link"
-
-  it "gets the committer's name"
 
   it "gets the committer's git hashes"
 
